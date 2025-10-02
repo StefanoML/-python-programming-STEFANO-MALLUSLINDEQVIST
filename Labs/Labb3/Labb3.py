47b0223144ffc8f9d026e3ab84968690c19817b8
StefanoML <stefano.malluslindeqvist@iths.se>
2025-10-02 17:29:01 +0200

    Finished the visualization and added the text files

--- Labs/Labb3/Labb3.py
+++ Labs/Labb3/Labb3.py
@@ -1,4 +1,58 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("unlabelled_data.csv")
-df.head()
+k= -1
+m= 0
+
+
+#This was the function I used in the first part of the assignment
+#to classify the points relative to the line I guessed
+def classify(row,k,m):  
+    point_x = row["X"]
+    point_y = row["Y"]
+    line_y = k*point_x +m
+    if line_y >= point_y: 
+        return 0
+    else: 
+        return 1
+#A function to visualize the line chosen and the points
+def plot(df):
+    x_min = (df["X"].min())
+    x_max = (df["X"].max())
+
+    y_start = k*x_min+m
+    y_end = k*x_max+m
+
+    df0 = df[df["Label"]==0]
+    df1 = df[df["Label"]==1]
+
+    plt.scatter(df0["X"],df0["Y"],c = "Orange", label = "Points Below the Line")
+    plt.scatter(df1["X"],df1["Y"], c = "DarkCyan", label = "Points Above the Line")
+    plt.plot([x_min,x_max],[y_start,y_end], c = "red", label="Classifier Line")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True)
+    plt.legend()
+
+    plt.show()
+
+
+#Finally, here's the core of the script
+def main():
+    try:
+        df = pd.read_csv("unlabelled_data.csv", header = None)
+    except FileNotFoundError:
+        print("The file 'unlabelled_data.csv' could not be found in the script folder.")
+
+    df = df.dropna()
+    df.rename(columns={0:"X",1:"Y"}, inplace=True)
+    
+
+    df["Label"] = df.apply(classify, axis=1, args=(k,m))
+    plot(df)
+
+    df.to_csv("labelled_data.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Labs/Labb3/README.md
@@ -0,0 +1,23 @@
+# English version:
+This is a quick explanation of the first version of the script.
+
+The only goal of the script is to read the points from the file unlabelled_data.csv and, calculated a line, classify these points based on their position compared to the line.
+
+Then it will create a new file in which the points are all labeled with a 0 if they're below or on the line, or a 1 if they're above.
+
+The only instruction needed to run this script is to make sure in the terminal that we are in the folder in which the file is.
+
+As a result for running the script we will get a visualization of the line and the points classified in different colors. 
+
+
+# Svensk Version:
+
+Dethär är en snabb förklaring av den första versionen av skriptet.
+
+Skriptets enda mål är att läsa in punkterna från filen unlabelled_data.csv och, utifrån en beräknad linje, klassificera dessa punkter baserat på deras position i förhållande till linjen.
+
+Därefter skapas en ny fil (labelled_data.csv) där alla punkter är märkta med etiketten 0 om de ligger under eller på linjen, eller 1 om de ligger ovanför linjen.
+
+Den enda instruktionen som behövs för att köra detta skript är att säkerställa att du befinner dig i mappen där filen ligger när du kör kommandot i terminalen.
+
+Som resultat av att köra skriptet får vi en visualisering av linjen och de klassificerade punkterna i olika färger.
